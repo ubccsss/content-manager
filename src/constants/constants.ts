ea8bd9d66fbc41387bef26d1dc3a4a68d52b9017
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2022-09-08 00:17:46 -0700

    Use constants based on NODE_ENV

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,20 @@
 export const MONTHS_MAP = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-export const OWNER = "csssbot";
-export const REPO = "testing";
-export const BASE_BRANCH = "main";
+// return GitHub details based on whether the environment is production or development
+const getGithubDetails = () => {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    return {
+      OWNER: "csssbot",
+      REPO: "testing",
+      BASE_BRANCH: "main"
+    };
+  } else {
+    return {
+      OWNER: "ubccsss",
+      REPO: "ubccsss.org",
+      BASE_BRANCH: "master"
+    };
+  }
+}
+
+export const { OWNER, REPO, BASE_BRANCH } = getGithubDetails();

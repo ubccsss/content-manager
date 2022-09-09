afe28a30308318b51d0cff59280c5732961c7141
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2022-09-09 00:53:32 -0700

    Improved Responsiveness of AlertComponent

--- src/components/AlertComponent.tsx
+++ src/components/AlertComponent.tsx
@@ -1,4 +1,4 @@
-import {Alert} from "react-bootstrap";
+import {Alert, Col, Container, Row} from "react-bootstrap";
 import {updateAlertShow} from "../reducers/AlertActions";
 import React from "react";
 import {useDispatch, useStore} from "../contexts/contexts";
@@ -9,20 +9,25 @@ export const AlertComponent = () => {
 
   return (
     show ? (
-      <div className="w-100 d-flex align-items-center justify-content-center position-fixed">
-          <Alert variant={variant}
-            onClose={() => dispatch(updateAlertShow(false))}
-            dismissible style={{maxWidth: "75%"}}
-          >
-            <div dangerouslySetInnerHTML={{__html: message}}/>
-            {url && urlText && (
-              <>
-                <br/>
-                <Alert.Link href={url}>{urlText}</Alert.Link>
-              </>
-            )}
-          </Alert>
-      </div>
+      <Container className="position-fixed ps-0 pe-5 pt-5" fluid>
+        <Row className="justify-content-center">
+          <Col xs={10} sm={8} md={6} lg={5} xl={4}>
+            <Alert variant={variant}
+                   onClose={() => dispatch(updateAlertShow(false))}
+                   dismissible
+                   className="mx-auto"
+            >
+              <div dangerouslySetInnerHTML={{__html: message}}/>
+              {url && urlText && (
+                <>
+                  <br/>
+                  <Alert.Link href={url}>{urlText}</Alert.Link>
+                </>
+              )}
+            </Alert>
+          </Col>
+        </Row>
+      </Container>
     ) : null
   )
 }

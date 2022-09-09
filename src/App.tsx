9d667c333a30739bcef651c7dd79a2d2fdfd9973
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2022-09-08 19:44:26 -0700

    Created an independent Alert component to clear up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,17 @@
 import React from 'react';
 import './App.css';
-import {Alert, Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import {FormComponent, MarkdownPreview, OutputPreview} from "./components";
-import {formInitialState, useDispatch, useStore} from './contexts/contexts';
-import {updateAlertShow} from "./reducers/AlertActions";
+import {formInitialState} from './contexts/contexts';
 import {RepoIcons} from "./components/RepoIcons";
+import {AlertComponent} from "./components/AlertComponent";
 
 const App = () => {
   const [isMarkdownPreview, setIsMarkdownPreview] = React.useState(true);
-  const dispatch = useDispatch();
-  const store = useStore();
-  const alert = store.alert;
 
   return (
     <>
-      {
-        alert.show &&
-          <div className="w-100 d-flex align-items-center justify-content-center position-fixed">
-              <Alert
-                  variant={alert.variant}
-                  onClose={() => dispatch(updateAlertShow(false))}
-                  dismissible style={{maxWidth: "75%"}}
-              >
-                  <div dangerouslySetInnerHTML={{__html: alert.message}}/>
-                {alert.url && alert.urlText && (
-                  <>
-                    <br/>
-                    <Alert.Link href={alert.url}>{alert.urlText}</Alert.Link>
-                  </>
-                )}
-              </Alert>
-          </div>
-      }
+      <AlertComponent/>
       <Container fluid>
         <Row xs={1} lg={2} className="row-height">
           <Col className="scrollable pb-4 pb-lg-0">

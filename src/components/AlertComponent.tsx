2fd190e2ec3b98ad020124753745e56dec587d6a
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2022-09-14 12:22:22 -0700

    Added zIndex and padding in ScrollableCol to ensure that Body is below Header

--- src/components/AlertComponent.tsx
+++ src/components/AlertComponent.tsx
@@ -9,7 +9,7 @@ export const AlertComponent = () => {
 
   return (
     show ? (
-      <Container className="position-fixed ps-0 pe-5 pt-5 pe-none" style={{ zIndex: 1}} fluid>
+      <Container className="position-fixed ps-0 pe-5 pt-5 pe-none" style={{ zIndex: 2}} fluid>
         <Row className="justify-content-center">
           <Col xs={10} sm={8} md={6} lg={5} xl={4}>
             <Alert variant={variant}
